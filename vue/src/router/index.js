474ff156b2c1a45c3fa76cc9b06d6d0c848079aa
lzb <llzb12138@foxmail.com>
2022-08-02 11:52:13 +0800

    完整项目上传

---
+++ vue/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from '@/store'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Manage',
+    component: () => import('../views/Manage.vue'),
+    redirect: "/home",
+    children: [
+      {path:'home',name:'主页',component: ()=> import('../views/Home.vue')},
+      {path:'user',name:'用户管理',component: ()=> import('../views/User.vue')}
+
+
+    ]
+  },
+  {
+    path: '/about',
+    name: 'About',
+
+    component: () => import('../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+//路由守卫
+router.beforeEach((to,from,next)=>{
+  localStorage.setItem("currentPathName",to.name)  //设置当前的路由名称，为了在Header组件中去使用
+  store.commit("setPath")  //触发store的数据更新
+  next() //放行路由
+})
+
+export default router
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>这是主页</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
